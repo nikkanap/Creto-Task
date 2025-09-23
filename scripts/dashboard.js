46e7088b08ec58972a16e1d310438741d62b6e53
nikkanap <nenaputo@up.edu.ph>
2025-09-23 20:13:25 +0800

    Add js code for displaying the overlays and fix the background-fade

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,6 +1,50 @@
-const date = document.querySelector('.js-23');
-date.addEventListener('click', () => {
-    console.log('this is the 23rd of september');
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
+
+function renderDashboard() {
+    const today = dayjs();
+    let date = today.format('D');
+    console.log(date);
+
+    let calendarHTML = '';
+    let days = 1;
+    for(let i = 1; i <= 5; i++) {
+        calendarHTML += '<tr>';
+        for(let j = 0; j < 7; j++){
+            calendarHTML += `
+            <td class="
+            ${(days > 31) ? 'none' : `js-${days}`} 
+            ${(days === date) ? 'today' : ''}
+            ${(days < date) ? 'previous-days' : ''}
+            ">
+                ${(days > 31) ? '' : days++}
+            </td>`;
+        }
+        calendarHTML += '</tr>';
+    }
+    document.querySelector('.js-calendar').innerHTML = calendarHTML;
+
+    const todayCell = document.querySelector('.js-23');
+    todayCell.addEventListener('click', () => {
+        console.log('this is the 23rd of september');
+        openOverlays();
+        makeTextAreaEditable();
+    });
+
+    document.querySelectorAll('.previous-days').forEach((prevDay) => {
+        prevDay.addEventListener('click', () => {
+            openOverlays();
+            makeTextAreaReadOnly();
+        });
+    });
+
+    const closeButton = document.querySelector('.js-journal-close-button');
+    closeButton.addEventListener('click', () => {
+        closeOverlays();
+    });
+}
+renderDashboard();
+
+function openOverlays() {
     console.log('opening the overlay');
     const backgroundFade = document.querySelector('.js-background-fade');
     backgroundFade.classList.add('display-content');
@@ -9,10 +53,9 @@ date.addEventListener('click', () => {
     const overlayDiv = document.querySelector('.js-overlay-div');
     overlayDiv.classList.add('display-content');
     console.log(overlayDiv);
-});
+}
 
-const closeButton = document.querySelector('.js-journal-close-button');
-closeButton.addEventListener('click', () => {
+function closeOverlays() {
     console.log('closing the overlay');
     const backgroundFade = document.querySelector('.js-background-fade');
     backgroundFade.classList.remove('display-content');
@@ -21,4 +64,12 @@ closeButton.addEventListener('click', () => {
     const overlayDiv = document.querySelector('.js-overlay-div');
     overlayDiv.classList.remove('display-content');
     console.log(overlayDiv);
-});
+}
+
+function makeTextAreaReadOnly() {
+    document.querySelector('.journal-entry-field').readOnly = true;
+}
+
+function makeTextAreaEditable() {
+    document.querySelector('.journal-entry-field').readOnly = false;
+}
