1d1cf75f6115481b91ab94a02c8b18de1d6b3822
nikkanap <nenaputo@up.edu.ph>
2025-09-23 14:34:19 +0800

    Add eventListeners to 23 and x button in dashboard.js

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,9 +1,24 @@
 const date = document.querySelector('.js-23');
 date.addEventListener('click', () => {
     console.log('this is the 23rd of september');
-})
+    console.log('opening the overlay');
+    const backgroundFade = document.querySelector('.js-background-fade');
+    backgroundFade.classList.add('display-content');
+    console.log(backgroundFade);
+
+    const overlayDiv = document.querySelector('.js-overlay-div');
+    overlayDiv.classList.add('display-content');
+    console.log(overlayDiv);
+});
 
 const closeButton = document.querySelector('.js-journal-close-button');
 closeButton.addEventListener('click', () => {
     console.log('closing the overlay');
+    const backgroundFade = document.querySelector('.js-background-fade');
+    backgroundFade.classList.remove('display-content');
+    console.log(backgroundFade);
+
+    const overlayDiv = document.querySelector('.js-overlay-div');
+    overlayDiv.classList.remove('display-content');
+    console.log(overlayDiv);
 });
