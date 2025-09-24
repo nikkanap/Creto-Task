af0d0f9c2e883ed61851c4c2f1675246ab295f81
nikkanap <nenaputo@up.edu.ph>
2025-09-24 10:56:57 +0800

    Fix calendar functionality using dayjs

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,42 +1,66 @@
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
 
 function renderDashboard() {
-    const today = dayjs();
-    let date = today.format('D');
-    console.log(date);
-
+    let { month, date, year } = getCurrentDate();
+    document.querySelector('.js-month').innerHTML = `${month} - ${year}`;
+    
+    // setting up the dates in the calendar (rendering html)
     let calendarHTML = '';
-    let days = 1;
+    // weekdates
+    const weekdates = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    calendarHTML += '<tr>'
+    weekdates.forEach((date) => {
+        calendarHTML += `<th class="table-header">${date}</th>`;
+    });
+    calendarHTML += '</tr>'
+    
+    // actual dates
+    let day = 1;
     for(let i = 1; i <= 5; i++) {
         calendarHTML += '<tr>';
         for(let j = 0; j < 7; j++){
-            calendarHTML += `
-            <td class="
-            ${(days > 31) ? 'none' : `js-${days}`} 
-            ${(days === date) ? 'today' : ''}
-            ${(days < date) ? 'previous-days' : ''}
-            ">
-                ${(days > 31) ? '' : days++}
-            </td>`;
+            // we have to start from 1 to arrange things
+            if(getDayOfTheWeekNumber(day, month, year) != j){
+                calendarHTML += '<td></td>';
+            } else {
+                calendarHTML += `
+                <td class="
+                    ${(day === Number(date)) ? 'today js-today' : ''}
+                    ${(day < Number(date)) ? 'js-previous-days' : ''}
+                " data-day="${day}">
+                    ${(day > getNoOfDaysInAMonth(month, year)) ? '' : `${day} <br> Tasks Finished: N/A`}
+                </td>`;
+                day++;
+            }
         }
         calendarHTML += '</tr>';
     }
+    console.log(calendarHTML);
     document.querySelector('.js-calendar').innerHTML = calendarHTML;
 
-    const todayCell = document.querySelector('.js-23');
+    // calendar date functionalities
+    const todayCell = document.querySelector('.js-today');
     todayCell.addEventListener('click', () => {
-        console.log('this is the 23rd of september');
         openOverlays();
         makeTextAreaEditable();
+        todayCell.classList.add('selected');
+        
+        const { day } = todayCell.dataset;
+        renderJournalEntry(day, month, year);
     });
 
-    document.querySelectorAll('.previous-days').forEach((prevDay) => {
+    document.querySelectorAll('.js-previous-days').forEach((prevDay) => {
         prevDay.addEventListener('click', () => {
             openOverlays();
             makeTextAreaReadOnly();
+            prevDay.classList.add('selected');
+            
+            const { day } = prevDay.dataset;
+            renderJournalEntry(day, month, year);
         });
     });
 
+    // close button functionality
     const closeButton = document.querySelector('.js-journal-close-button');
     closeButton.addEventListener('click', () => {
         closeOverlays();
@@ -73,3 +97,30 @@ function makeTextAreaReadOnly() {
 function makeTextAreaEditable() {
     document.querySelector('.journal-entry-field').readOnly = false;
 }
+
+function renderJournalEntry(day, month, year) {
+    let selectedDate = dayjs(`${month} ${day}, ${year}`, 'MMMM D, YYYY');
+    let dateWithWeek = selectedDate.format('dddd - MMMM D, YYYY')
+    console.log(dateWithWeek);
+    document.querySelector('.js-journal-date').innerHTML = dateWithWeek;
+}
+
+function getCurrentDate() {
+    const fullDate = dayjs();
+    const month = fullDate.format('MMMM');
+    const year = fullDate.format('YYYY');
+    const date = fullDate.format('D');
+    return { month, date, year };
+}
+
+function getDayOfTheWeekNumber(day, month, year) {
+    let selectedDate = dayjs(`${month} ${day}, ${year}`, 'MMMM D, YYYY');
+    return Number(selectedDate.format('d'));
+}
+
+function getNoOfDaysInAMonth(month, year) {
+    let selectedMonth = dayjs(`${month}-${year}`, 'MMMM YYYY');
+    return selectedMonth.daysInMonth();
+}
+
+
