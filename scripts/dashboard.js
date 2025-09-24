5b694a9a6725ce2f3958009df0afe4f853b17983
nikkanap <nenaputo@up.edu.ph>
2025-09-24 11:26:44 +0800

    Separate date functions to a different file (dates.js)

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,20 +1,24 @@
-import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
+import { getCurrentDate, getDayOfTheWeekNumber, getNoOfDaysInAMonth, getFullDate } from "./utils/dates.js";
 
 function renderDashboard() {
     let { month, date, year } = getCurrentDate();
     document.querySelector('.js-month').innerHTML = `${month} - ${year}`;
     
-    // setting up the dates in the calendar (rendering html)
+    // rendering the calendar (html)
     let calendarHTML = '';
     // weekdates
     const weekdates = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     calendarHTML += '<tr>'
     weekdates.forEach((date) => {
-        calendarHTML += `<th class="table-header">${date}</th>`;
+        calendarHTML += `
+            <th class="table-header">
+                ${date}
+            </th>
+        `;
     });
     calendarHTML += '</tr>'
     
-    // actual dates
+    // rendering the actual dates
     let day = 1;
     for(let i = 1; i <= 5; i++) {
         calendarHTML += '<tr>';
@@ -38,28 +42,24 @@ function renderDashboard() {
     console.log(calendarHTML);
     document.querySelector('.js-calendar').innerHTML = calendarHTML;
 
-    // calendar date functionalities
-    const todayCell = document.querySelector('.js-today');
-    todayCell.addEventListener('click', () => {
-        openOverlays();
-        makeTextAreaEditable();
-        todayCell.classList.add('selected');
-        
-        const { day } = todayCell.dataset;
-        renderJournalEntry(day, month, year);
-    });
+    document.querySelectorAll('td').forEach((dayCell) => {
+        dayCell.addEventListener('click', () => {
+            // refuse opening the overlays for any invalid cells
+            if(dayCell.classList.length === 0){
+                return;
+            }
 
-    document.querySelectorAll('.js-previous-days').forEach((prevDay) => {
-        prevDay.addEventListener('click', () => {
+            dayCell.classList.add('selected');
             openOverlays();
-            makeTextAreaReadOnly();
-            prevDay.classList.add('selected');
-            
-            const { day } = prevDay.dataset;
+            document.querySelector('.journal-entry-field')
+            .readOnly = (dayCell.classList.contains('js-today')) ? false : true;
+
+            const { day } = dayCell.dataset;
             renderJournalEntry(day, month, year);
         });
     });
 
+    // ---- journal portion functionalities ----
     // close button functionality
     const closeButton = document.querySelector('.js-journal-close-button');
     closeButton.addEventListener('click', () => {
@@ -69,58 +69,25 @@ function renderDashboard() {
 renderDashboard();
 
 function openOverlays() {
-    console.log('opening the overlay');
     const backgroundFade = document.querySelector('.js-background-fade');
     backgroundFade.classList.add('display-content');
-    console.log(backgroundFade);
 
     const overlayDiv = document.querySelector('.js-overlay-div');
     overlayDiv.classList.add('display-content');
-    console.log(overlayDiv);
 }
 
 function closeOverlays() {
-    console.log('closing the overlay');
     const backgroundFade = document.querySelector('.js-background-fade');
     backgroundFade.classList.remove('display-content');
-    console.log(backgroundFade);
 
     const overlayDiv = document.querySelector('.js-overlay-div');
     overlayDiv.classList.remove('display-content');
-    console.log(overlayDiv);
-}
-
-function makeTextAreaReadOnly() {
-    document.querySelector('.journal-entry-field').readOnly = true;
-}
-
-function makeTextAreaEditable() {
-    document.querySelector('.journal-entry-field').readOnly = false;
 }
 
 function renderJournalEntry(day, month, year) {
-    let selectedDate = dayjs(`${month} ${day}, ${year}`, 'MMMM D, YYYY');
-    let dateWithWeek = selectedDate.format('dddd - MMMM D, YYYY')
-    console.log(dateWithWeek);
+    let dateWithWeek = getFullDate(month, day, year);
     document.querySelector('.js-journal-date').innerHTML = dateWithWeek;
 }
 
-function getCurrentDate() {
-    const fullDate = dayjs();
-    const month = fullDate.format('MMMM');
-    const year = fullDate.format('YYYY');
-    const date = fullDate.format('D');
-    return { month, date, year };
-}
-
-function getDayOfTheWeekNumber(day, month, year) {
-    let selectedDate = dayjs(`${month} ${day}, ${year}`, 'MMMM D, YYYY');
-    return Number(selectedDate.format('d'));
-}
-
-function getNoOfDaysInAMonth(month, year) {
-    let selectedMonth = dayjs(`${month}-${year}`, 'MMMM YYYY');
-    return selectedMonth.daysInMonth();
-}
 
 
---
+++ scripts/utils/dates.js
@@ -0,0 +1,24 @@
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
+
+export function getCurrentDate() {
+    const fullDate = dayjs();
+    const month = fullDate.format('MMMM');
+    const year = fullDate.format('YYYY');
+    const date = fullDate.format('D');
+    return { month, date, year };
+}
+
+export function getDayOfTheWeekNumber(day, month, year) {
+    let selectedDate = dayjs(`${month} ${day}, ${year}`, 'MMMM D, YYYY');
+    return Number(selectedDate.format('d'));
+}
+
+export function getNoOfDaysInAMonth(month, year) {
+    let selectedMonth = dayjs(`${month}-${year}`, 'MMMM YYYY');
+    return selectedMonth.daysInMonth();
+}
+
+export function getFullDate(month, day, year) {
+    let selectedDate = dayjs(`${month} ${day}, ${year}`, 'MMMM D, YYYY');
+    return selectedDate.format('dddd - MMMM D, YYYY');
+}
